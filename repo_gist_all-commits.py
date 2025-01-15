17f2b83f4a95f0cc5624d874c0c68b9cf9ddaa5a
Y2hhbmRhbiBwYXRoYWs= <71752348+xerocorps@users.noreply.github.com>
2025-01-15 14:00:12 +0530

    Update repo_gist_all-commits.py

--- repo_gist_all-commits.py
+++ repo_gist_all-commits.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
-# python3 ./all-github.py -u <user> -d <directory>
+# python3 ./all-git-commits.py -u <user> -d <directory> -r <repo> -n <number of commits>
 #
-# Tool to inspect Github and pull local copies of all repos and gists
+# Tool to inspect Github and pull local copies of all commits for
 # for the given user.
 #
 # Why?  Every ask yourself the question 'I know I did this before, but
-#       which repos was it in?'
+#       which repos commit was it in?'
 # With all files local, you can use any serach-in-files app to look.
 #
-# Companion App: all-git-commits.py pull commits for a repo.
+# Companion App: all-gethub.py to get all repos for a user.
 
 import getopt
 import json
@@ -20,22 +20,34 @@ import sys
 from urllib.request import urlopen
 
 def Usage():
-  print("Usage: %s -u <github user> -d <directory>" % sys.argv[0])
+  print("Usage: %s -u <github user> -d <directory> -r <repo> -n <number of commits>" % sys.argv[0])
   print("  -u <github user>  github user name")
-  print("  -d <directory>    local directory for repos and gists")
+  print("  -d <directory>    local directory for repos commits")
+  print("  -r <repo>         specific repo")
+  print("  -n <number>       (optional) number of commits (newest to oldest)")
+  print("  -l <listonly>     (optional) list only, no checkout")
 
 def main():
 
   githubUser  = ''
   destDirectory = ''
+  currentRepo = ''
+  numberCommits = 10000000
+  listOnly = False
   try:
     # process command arguments
-    ouropts, args = getopt.getopt(sys.argv[1:],"u:d:h")
+    ouropts, args = getopt.getopt(sys.argv[1:],"u:d:r:n:lh")
     for o, a in ouropts:
       if   o == '-u':
         githubUser = a
       if   o == '-d':
         destDirectory = a
+      if   o == '-r':
+        currentRepo = a
+      if   o == '-n':
+        numberCommits = int(a)
+      if   o == '-l':
+        listOnly = True
       elif o == '-h':
         Usage()
         sys.exit(0)
@@ -52,43 +64,37 @@ def main():
       print("please use -d for local directory")
       Usage()
       sys.exit(0)
+  if type(currentRepo) != str or len(currentRepo) <= 0:
+      print("please use -r for repo")
+      Usage()
+      sys.exit(0)
 
-
-  reposLink = "https://api.github.com/users/{0}/repos?type=all&per_page=100&page=1".format(githubUser)
-  f = urlopen(reposLink)
-  repos = json.loads(f.readline())
-  print("total repos: {0}".format(len(repos)))
-
-  gistsLink = "https://api.github.com/users/{0}/gists".format(githubUser)
-  f = urlopen(gistsLink)
-  gists = json.loads(f.readline())
-  print("total gists: {0}".format(len(gists)))
+  commitsLink = "https://api.github.com/repos/{0}/{1}/commits".format(githubUser, currentRepo)
+  f = urlopen(commitsLink)
+  commits = json.loads(f.readline())
+  print("repo: '{0}' ; total commits: {1}".format(currentRepo, len(commits)))
 
   os.mkdir(destDirectory)
-  os.mkdir(os.path.join(destDirectory, 'repos'))
-  os.mkdir(os.path.join(destDirectory, 'gists'))
+  os.chdir(destDirectory)
 
-  print('cd repos')
-  os.chdir(os.path.join('.', destDirectory, 'repos'))
-  with open('repos.json', 'w') as outfile:
-    json.dump(repos, outfile, indent=2)
-  for repo in repos:
-    print(repo['html_url'])
-    subprocess.call(['git', 'clone', repo['html_url']])
-    print()
-  print('cd ..')
-  os.chdir(os.path.join('..', '..'))
+  repoLink = "https://github.com/{0}/{1}.git".format(githubUser, currentRepo)
+  count = 0
+  for commit in commits:
+    d = commit['commit']['committer']['date']
+    name = commit['commit']['committer']['name']
+    sha = commit['sha']
+    subdir = "{0}-{1}-{2}-{3}".format(currentRepo, d, name, sha)
+    print(subdir)
+    if not listOnly:
+      subprocess.call(['git', 'clone', repoLink, subdir])
+      os.chdir(subdir)
+      subprocess.call(['git', 'checkout', sha])
+      os.chdir(os.path.join('..'))
 
-  print('cd gists')
-  os.chdir(os.path.join('.', destDirectory, 'gists'))
-  with open('gists.json', 'w') as outfile:
-    json.dump(gists, outfile, indent=2)
-  for gist in gists:
-    print(gist['git_pull_url'])
-    subprocess.call(['git', 'clone', gist['git_pull_url']])
-    print()
-  print('cd ..')
-  os.chdir(os.path.join('..', '..'))
+    # are we done?
+    count += 1
+    if (count >= numberCommits):
+      break
 
 
 if __name__ == "__main__":
